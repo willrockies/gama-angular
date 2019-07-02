538493ea07bb10de7fdb6309d49d8e886ee28057
unknown <Administrator@WIN-FB05DLQJ7B.corp.avanade.org>
2019-07-01 21:42:17 -0300

    Aula 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'gama-angular-ex';
+export class AppComponent implements OnInit {
+  title = "Wilson";
+  sobrenome = "Alves";
+  contador = 0;
+  items = ["2", "3", "4", "5"];
+  ngOnInit() {
+    setInterval(() => {
+      this.title = "Wilson";
+    }, 1000);
+  }
+
+  novoItem() {
+    const text = prompt("Digite um nome");
+
+    this.items.push(text);
+  }
 }
